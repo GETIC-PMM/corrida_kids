d9ce345182146877b03929765d0314a6060c69cd
Giovanni Martins <gmgit10@gmail.com>
2024-10-07 20:57:51 -0300

    correção na migration

--- db/migrate/20241007135733_remove_old_params_from_inscricoes.rb
+++ db/migrate/20241007135733_remove_old_params_from_inscricoes.rb
@@ -1,13 +1,13 @@
 class RemoveOldParamsFromInscricoes < ActiveRecord::Migration[7.0]
   def change
-    remove_column :inscricoes, :prova, :string
-    remove_column :inscricoes, :sub_categoria, :string
+    remove_column :inscricoes, :prova
+    remove_column :inscricoes, :sub_categoria
     remove_column :inscricoes, :entregar, :boolean
-    remove_column :inscricoes, :nome, :string
-    remove_column :inscricoes, :email, :string
-    remove_column :inscricoes, :whatsapp, :string
-    remove_column :inscricoes, :matricula, :string
-    remove_column :inscricoes, :categoria, :string
-    remove_column :inscricoes, :tamanho_camisa, :string
+    remove_column :inscricoes, :nome
+    remove_column :inscricoes, :email
+    remove_column :inscricoes, :whatsapp
+    remove_column :inscricoes, :matricula
+    remove_column :inscricoes, :categoria
+    remove_column :inscricoes, :tamanho_camisa
   end
 end
